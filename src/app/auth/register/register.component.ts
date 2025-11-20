cff5108053473b23fe8479ac630670c1e66ff756
sami09-cyber <bagaassami009@gmail.com>
2025-11-20 11:18:13 +0100

    My contribution 1

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
@@ -9,28 +9,85 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+   registerForm: FormGroup;
+   errorMessage = "";
 
-  constructor(private readonly authService:AuthService){}
-  private readonly router:Router=inject(Router);
-  
-    register=new FormGroup({
-      email:new FormControl('',[Validators.email,Validators.required]),
-      password:new FormControl('',[Validators.maxLength(15),Validators.minLength(4),Validators.required])
-    })
-  
-    async signUp(){
-  
-      if (this.register.valid) {
-        try{
-          const auth=await this.authService.signUp({email:this.register.get('email')?.value,password:this.register.get('password')?.value});
-          if (auth.user) {
-            this.register.reset();
-            this.router.navigateByUrl('/login');
-          }
-        }catch(err){
-  
-          console.log(err);
-        }
+   constructor(private fb: FormBuilder, private authService: AuthService,private readonly router:Router){
+      this.registerForm = this.fb.group({
+         firstname: ['', [
+            Validators.required,
+            Validators.minLength(2)
+         ]],
+         lastname: ['', [
+            Validators.required,
+            Validators.minLength(2)
+         ]],
+         email:['user@gmail.com',[
+            Validators.required,
+            Validators.email,
+            Validators.pattern('^[a-zA-Z0-9._%+-]+@(?:gmail\\.com|hotmail\\.com|outlook\\.com|yahoo\\.com|yahoo\\.fr)$')
+         ]],
+         password:['user1234',[
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(10),
+            Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z]).*$')
+         ]
+         ]
+      });
+   }
+
+   async signUp(){
+
+      console.log("Statut du formulaire : ", this.registerForm.status);
+      console.log("Erreurs firstname : ", this.firstname?.errors);
+      console.log("Erreurs lastname : ", this.lastname?.errors);
+      console.log("Erreurs email : ", this.email?.errors);
+      console.log("Erreurs password : ", this.password?.errors);
+
+      this.registerForm.markAllAsTouched();
+
+      if (this.registerForm.valid) {
+         try{
+            const auth=await this.authService.signIn({
+               email:this.registerForm.get('email')?.value,
+               password:this.registerForm.get('password')?.value});
+            if (auth.user) {
+               this.router.navigateByUrl('/');
+               this.registerForm.reset();
+            }
+         }catch(err){
+            console.log(err);
+            this.errorMessage ="Identifiants Incorrects."
+         }
       }
-    }
+   }
+
+   async signInGoogle(){
+      try {
+         const res= await this.authService.signInGoogle();
+         if (res.user) {
+            console.log(res.user.email);
+            this.router.navigateByUrl('/');
+         }
+      } catch (error) {
+         console.error(error);
+      }
+   }
+
+   get lastname() {
+      return this.registerForm.get('lastname');
+   }
+
+   get firstname() {
+      return this.registerForm.get('firstname');
+   }
+
+   get email() {
+      return this.registerForm.get('email');
+   }
+
+   get password(){
+      return this.registerForm.get('password');
+   }
 }
